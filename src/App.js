32fe00130a4703eb41f848b6441f4ec86520a60c
Yousub Ali <97525491+YsbAli@users.noreply.github.com>
2022-10-29 10:21:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Viewars</h1>
+      <p>New Repo for create projects</p>
     </div>
   );
 }
